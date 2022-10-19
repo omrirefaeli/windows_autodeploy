3f60dc65cc895e2496a43da876c888fa08304874
Omri Refaeli <omrirefaeli@microsoft.com>
2022-10-19 12:32:00 +0300

    Add Terminal shortcut

--- autodeploy.ps1
+++ autodeploy.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
-
+#################################### Chocolately ##############################################
 Write-Host -ForegroundColor black -BackgroundColor Cyan "installing chocolately & packages"
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
@@ -22,6 +22,7 @@ $install_array | %{
 choco install $_ -y
 }
 
+#################################### RC Revert ##############################################
 Write-Host -ForegroundColor black -BackgroundColor Cyan "Reverting the new Right Click menu to the old Windows 10 one"
 
 try {
@@ -45,4 +46,97 @@ catch {
     else {
         Write-Host -ForegroundColor Red -BackgroundColor Black "Revert Right Click registry change did not implement successfully"
     }
+}
+
+
+#################################### Elevated Terminal Shortcut ##############################################
+CreateShortcut -name "Terminal Admin" -Target "C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.15.2875.0_x64__8wekyb3d8bbwe\wt.exe" -OutputDirectory "C:\Users\omrirefaeli\Desktop" -Elevated True -HotKey "CTRL+ALT+T"
+
+
+
+
+
+#################################### Help Functions ##############################################
+Function CreateShortcut
+{
+    [CmdletBinding()]
+    param (    
+        [parameter(Mandatory=$true)]
+        [ValidateScript( {[IO.File]::Exists($_)} )]
+        [System.IO.FileInfo] $Target,
+    
+        [ValidateScript( {[IO.Directory]::Exists($_)} )]
+        [System.IO.DirectoryInfo] $OutputDirectory,
+    
+        [string] $Name,
+        [string] $Description,
+    
+        [string] $Arguments,
+        [System.IO.DirectoryInfo] $WorkingDirectory,
+    
+        [string] $HotKey,
+        [int] $WindowStyle = 1,
+        [string] $IconLocation,
+        [switch] $Elevated
+    )
+
+    try {
+        #region Create Shortcut
+        if ($Name) {
+            [System.IO.FileInfo] $LinkFileName = [System.IO.Path]::ChangeExtension($Name, "lnk")
+        } else {
+            [System.IO.FileInfo] $LinkFileName = [System.IO.Path]::ChangeExtension($Target.Name, "lnk")
+        }
+    
+        if ($OutputDirectory) {
+            [System.IO.FileInfo] $LinkFile = [IO.Path]::Combine($OutputDirectory, $LinkFileName)
+        } else {
+            [System.IO.FileInfo] $LinkFile = [IO.Path]::Combine($Target.Directory, $LinkFileName)
+        }
+
+       
+        $wshshell = New-Object -ComObject WScript.Shell
+        $shortCut = $wshShell.CreateShortCut($LinkFile) 
+        $shortCut.TargetPath = $Target
+        $shortCut.WindowStyle = $WindowStyle
+        $shortCut.Description = $Description
+        $shortCut.WorkingDirectory = $WorkingDirectory
+        $shortCut.HotKey = $HotKey
+        $shortCut.Arguments = $Arguments
+        if ($IconLocation) {
+            $shortCut.IconLocation = $IconLocation
+        }
+        $shortCut.Save()
+        #endregion
+
+        #region Elevation Flag
+        if ($Elevated) {
+            $tempFileName = [IO.Path]::GetRandomFileName()
+            $tempFile = [IO.FileInfo][IO.Path]::Combine($LinkFile.Directory, $tempFileName)
+        
+            $writer = new-object System.IO.FileStream $tempFile, ([System.IO.FileMode]::Create)
+            $reader = $LinkFile.OpenRead()
+        
+            while ($reader.Position -lt $reader.Length)
+            {        
+                $byte = $reader.ReadByte()
+                if ($reader.Position -eq 22) {
+                    $byte = 34
+                }
+                $writer.WriteByte($byte)
+            }
+        
+            $reader.Close()
+            $writer.Close()
+        
+            $LinkFile.Delete()
+        
+            Rename-Item -Path $tempFile -NewName $LinkFile.Name
+        }
+        #endregion
+    } catch {
+        Write-Error "Failed to create shortcut. The error was '$_'."
+        return $null
+    }
+    return $LinkFile
 }

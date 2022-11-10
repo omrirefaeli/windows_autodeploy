ef487219a1438c71a49e987f5b8aa3c77333f397
Omri Refaeli <omrirefaeli@microsoft.com>
2022-11-10 17:51:25 +0200

    changed order of functions

--- autodeploy.ps1
+++ autodeploy.ps1
@@ -1,85 +1,9 @@
+#################################### Run this line first  ##############################################
 Set-ExecutionPolicy Bypass -Scope Process -Force
-#################################### Chocolately ##############################################
-Write-Host -ForegroundColor black -BackgroundColor Cyan "installing chocolately & packages"
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$install_array = @("googlechrome", "7zip.install", "git", "notepadplusplus", "everything", "adobereader")
-
-$personal_additions = @("evernote")
-Write-Host -ForegroundColor black -BackgroundColor Cyan "Would you like to include the personal software additions? The packages are:`n$($personal_additions)"
-do {
-    $personal_bool = Read-Host "Please enter [y/n]"
-}
-while ($personal_bool -notin @("y", "n"))
-
-if ($personal_bool -eq "y") { $install_array += $personal_additions } 
-
-$install_string = $install_array -join " "
-Write-Host -ForegroundColor black -BackgroundColor Cyan "Installing the packages: `n$($install_string)"
-
-$install_array | % {
-    choco install $_ -y
-}
-
-#################################### RC Revert ##############################################
-Write-Host -ForegroundColor black -BackgroundColor Cyan "Reverting the new Right Click menu to the old Windows 10 one"
-
-try {
-    $key = get-itemproperty "HKCU:\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32\"
-    if ($($key.psobject.properties | select -ExpandProperty Name ) -contains "(default)") {
-        Write-Host -ForegroundColor Green "Revert Right Click registry key already exists"
-    }
-    else {
-        throw [System.Management.Automation.ItemNotFoundException]
-    }
-  
-}
-catch {
-    new-item -path "HKCU:\SOFTWARE\CLASSES\CLSID" -name "{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Force
-    new-item -path "HKCU:\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -name "InprocServer32" -Force
-    New-ItemProperty  -path "HKCU:\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value ""
-    $key = get-itemproperty "HKCU:\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32\"
-    if ($($key.psobject.properties | select -ExpandProperty Name ) -contains "(default)") {
-        Write-Host -ForegroundColor Green "Revert Right Click registry change successfully implemented"
-    }
-    else {
-        Write-Host -ForegroundColor Red -BackgroundColor Black "Revert Right Click registry change did not implement successfully"
-    }
-}
-
-
-#################################### Elevated Terminal Shortcut ##############################################
-$terminal_path = (Get-ChildItem "C:\Program Files\WindowsApps\" -Include "wt.exe" -Recurse).FullName
-if (!$terminal_path) {
-    $terminal_path = (Get-ChildItem "C:\Program Files (x86)\WindowsApps\" -Include "wt.exe" -Recurse).FullName
-}
-if ($terminal_path) {
-    Write-Host -ForegroundColor Green -BackgroundColor Black "Elevated Terminal Shortcut: Found executable, creating a shortcut..."
-    CreateShortcut -name "Terminal Admin" -Target $terminal_path  -OutputDirectory "C:\Users\omrirefaeli\Desktop" -Elevated True -HotKey "CTRL+ALT+T"
-}
-else {
-    Write-Host -ForegroundColor Red -BackgroundColor Black "Elevated Terminal Shortcut: Could find Terminal executable, and therefore could not create a shortcut for the Terminal"
-}
-
-#################################### Notepad++ Shortcut ##############################################
-$notepadPath = "$env:HOMEDRIVE\Program Files\Notepad++\notepad++.exe"
-$notepadPath_86 = "$env:HOMEDRIVE\Program Files (x86)\Notepad++\notepad++.exe"
-$notepad_keys = "CTRL+SHIFT+N"
-if (Test-Path $notepadPath) {
-    Write-Host -ForegroundColor Green -BackgroundColor Black "Notepad++ Shortcut: Found executable, creating a shortcut... $notepad_keys"
-    CreateShortcut -name "Notepad++" -Target $notepadPath -OutputDirectory "$env:USERPROFILE\Desktop" -HotKey $notepad_keys
-}
-elseif (Test-Path $notepadPath_86) {
-    Write-Host -ForegroundColor Green -BackgroundColor Black "Notepad++ Shortcut: Found executable (x86), creating a shortcut... $notepad_keys"
-    CreateShortcut -name "Notepad++" -Target $notepadPath_86 -OutputDirectory "$env:USERPROFILE\Desktop" -HotKey $notepad_keys
-}
-else {
-    Write-Host -ForegroundColor Red -BackgroundColor Black "Notepad++ Shortcut: Could find Notepad++ executable, and therefore could not create a shortcut for the Terminal"
-
-}
-#################################### Enable WSL ##############################################
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux 
-
+#################################### Now run the entire script  ##############################################
+#
+#
 #################################### Help Functions ##############################################
 
 # https://blog.ctglobalservices.com/powershell/hra/create-shortcut-with-elevated-rights/ 
@@ -167,3 +91,85 @@ Function CreateShortcut {
     }
     return $LinkFile
 }
+
+#################################### Chocolately ##############################################
+Write-Host -ForegroundColor black -BackgroundColor Cyan "installing chocolately & packages"
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+$install_array = @("googlechrome", "7zip.install", "git", "notepadplusplus", "everything", "adobereader")
+
+$personal_additions = @("evernote")
+Write-Host -ForegroundColor black -BackgroundColor Cyan "Would you like to include the personal software additions? The packages are:`n$($personal_additions)"
+do {
+    $personal_bool = Read-Host "Please enter [y/n]"
+}
+while ($personal_bool -notin @("y", "n"))
+
+if ($personal_bool -eq "y") { $install_array += $personal_additions } 
+
+$install_string = $install_array -join " "
+Write-Host -ForegroundColor black -BackgroundColor Cyan "Installing the packages: `n$($install_string)"
+
+$install_array | % {
+    choco install $_ -y
+}
+
+#################################### RC Revert ##############################################
+Write-Host -ForegroundColor black -BackgroundColor Cyan "Reverting the new Right Click menu to the old Windows 10 one"
+
+try {
+    $key = get-itemproperty "HKCU:\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32\"
+    if ($($key.psobject.properties | select -ExpandProperty Name ) -contains "(default)") {
+        Write-Host -ForegroundColor Green "Revert Right Click registry key already exists"
+    }
+    else {
+        throw [System.Management.Automation.ItemNotFoundException]
+    }
+  
+}
+catch {
+    new-item -path "HKCU:\SOFTWARE\CLASSES\CLSID" -name "{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Force
+    new-item -path "HKCU:\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -name "InprocServer32" -Force
+    New-ItemProperty  -path "HKCU:\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value ""
+    $key = get-itemproperty "HKCU:\SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32\"
+    if ($($key.psobject.properties | select -ExpandProperty Name ) -contains "(default)") {
+        Write-Host -ForegroundColor Green "Revert Right Click registry change successfully implemented"
+    }
+    else {
+        Write-Host -ForegroundColor Red -BackgroundColor Black "Revert Right Click registry change did not implement successfully"
+    }
+}
+
+
+#################################### Elevated Terminal Shortcut ##############################################
+$terminal_path = (Get-ChildItem "C:\Program Files\WindowsApps\" -Include "wt.exe" -Recurse).FullName
+if (!$terminal_path) {
+    $terminal_path = (Get-ChildItem "C:\Program Files (x86)\WindowsApps\" -Include "wt.exe" -Recurse).FullName
+}
+if ($terminal_path) {
+    Write-Host -ForegroundColor Green -BackgroundColor Black "Elevated Terminal Shortcut: Found executable, creating a shortcut..."
+    CreateShortcut -name "Terminal Admin" -Target $terminal_path  -OutputDirectory "C:\Users\omrirefaeli\Desktop" -Elevated True -HotKey "CTRL+ALT+T"
+}
+else {
+    Write-Host -ForegroundColor Red -BackgroundColor Black "Elevated Terminal Shortcut: Could find Terminal executable, and therefore could not create a shortcut for the Terminal"
+}
+
+#################################### Notepad++ Shortcut ##############################################
+$notepadPath = "$env:HOMEDRIVE\Program Files\Notepad++\notepad++.exe"
+$notepadPath_86 = "$env:HOMEDRIVE\Program Files (x86)\Notepad++\notepad++.exe"
+$notepad_keys = "CTRL+SHIFT+N"
+if (Test-Path $notepadPath) {
+    Write-Host -ForegroundColor Green -BackgroundColor Black "Notepad++ Shortcut: Found executable, creating a shortcut... $notepad_keys"
+    CreateShortcut -name "Notepad++" -Target $notepadPath -OutputDirectory "$env:USERPROFILE\Desktop" -HotKey $notepad_keys
+}
+elseif (Test-Path $notepadPath_86) {
+    Write-Host -ForegroundColor Green -BackgroundColor Black "Notepad++ Shortcut: Found executable (x86), creating a shortcut... $notepad_keys"
+    CreateShortcut -name "Notepad++" -Target $notepadPath_86 -OutputDirectory "$env:USERPROFILE\Desktop" -HotKey $notepad_keys
+}
+else {
+    Write-Host -ForegroundColor Red -BackgroundColor Black "Notepad++ Shortcut: Could find Notepad++ executable, and therefore could not create a shortcut for the Terminal"
+
+}
+#################################### Enable WSL ##############################################
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux 
+
